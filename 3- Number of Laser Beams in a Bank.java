3ffc6cc950b033468bbd44200735ce2aeec76321
Youssef Ahmed <ybdalzym831@gmail.com>
2025-10-28 02:49:11 +0300

    Resovled Problem better: 2125. Number of Laser Beams in a Bank

--- 3- Number of Laser Beams in a Bank.java
+++ 3- Number of Laser Beams in a Bank.java
@@ -4,31 +4,16 @@ class Solution {
         int row1devices = 0;
         for (String bank1 : bank) {
             // get the number of Devices in the row
+            int numOfDevicesInRow = 0;
+            for(int j =0; j<bank1.length(); j++){
+                if(bank1.charAt(j)=='1') numOfDevicesInRow++;
+            }
             // if no security devices in the row continue
-            int numOfDevicesInRow = StringDegitsSum(bank1);
             if(numOfDevicesInRow == 0) continue;
             // laser beam between row1 and row2 = n of devices in row1 * n of devices in row2
-            if(row1devices == 0) row1devices = numOfDevicesInRow;
-            else {
-                result += row1devices * numOfDevicesInRow;
-                row1devices = numOfDevicesInRow;
-            }
+            result += row1devices * numOfDevicesInRow;
+            row1devices = numOfDevicesInRow;
         }
-        return result;
-    }
-
-    private static  int StringDegitsSum(String string) {
-        int result=0;
-        for(int i =0; i<string.length(); i++){
-            if(string.charAt(i) >='0' && string.charAt(i) <='9'){
-                result += (int)string.charAt(i)-48;
-            }
-            else{
-                System.out.println("can't convert string "+string+" to integer because it has charecter "+string.charAt(i));
-                return 0;
-            }
-        }
-
         return result;
     }
 }
